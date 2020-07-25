828c682dcb69ee33626f97b462fa686467dc0b1d
habibekayman <habibekayman@gmail.com>
2020-07-25 13:17:03 -0600

    first commit

---
+++ src/day54_Maps/Test.java
@@ -0,0 +1,19 @@
+package day54_Maps;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Color myFaveColor1 = Color.Blue;
+      //  Color myFaveColor2 = Color.Yellow;
+      //  Color myFaveColor3 = Color.White;
+
+        System.out.println( myFaveColor1 );
+
+
+        Browsers browseName = Browsers.Chrome;
+
+        System.out.println(browseName);
+
+
+    }
+}
